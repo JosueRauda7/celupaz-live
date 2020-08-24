eee4dd52c80507a06fbeac11dc8120e7db3c88a9
JosueRauda7 <katerozarack@gmail.com>
2020-08-23 19:27:19 -0600

    Identificar puerto para producción

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import socketIOClient from "socket.io-client";
 
+const puerto = process.env.PORT || 3000;
+
 // Components
 import HolaMundo from "./components/HolaMundo/HolaMundo";
 
 const App = (props) => {
 	useEffect(() => {
-		const socket = socketIOClient("http://localhost:3000/");
+		const socket = socketIOClient(`http://localhost:${puerto}/`);
 		socket.on("connect", () => {
 			console.log("hola");
 			socket.on("saludo", () => {
@@ -16,7 +18,7 @@ const App = (props) => {
 		});
 	});
 
-	return <h1>Hola</h1>;
+	return <HolaMundo />;
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
