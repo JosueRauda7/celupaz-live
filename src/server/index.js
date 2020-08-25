64462f076587d26ee926dfee72fd32cdd316c4d2
JosueRauda7 <katerozarack@gmail.com>
2020-08-24 21:38:51 -0600

    Prueba de produccion

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const socketIO = require("socket.io");
 const path = require("path");
-const http = require("http");
+const http = require("https");
 
 const app = express();
 let server = http.createServer(app);
