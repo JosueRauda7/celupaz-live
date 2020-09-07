25468c07c882c3226632a1de08a014bcfcab6b64
JosueRauda7 <katerozarack@gmail.com>
2020-09-07 06:42:09 -0600

    Reajuste de state en página Inicio

--- src/components/Muro/Muro.js
+++ src/components/Muro/Muro.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Muro.css";
 
 // Componentes
@@ -6,39 +6,11 @@ import PublicarBox from "../PublicarBox/PublicarBox";
 import RemaList from "../RemaList/RemaList";
 
 const Muro = (props) => {
-	const [state, setState] = useState({
-		remas: [
-			{
-				id: 0,
-				usuario: "Josué Rauda",
-				rema: "Todo lo que respire alabe al Señor",
-			},
-			{
-				id: 1,
-				usuario: "Katya Carrillo",
-				rema:
-					"Anhelo el día que venga Jesús porque ese día llegaré a ver su rostro",
-			},
-			{
-				id: 2,
-				usuario: "Andrea Clemente",
-				rema:
-					"Ama al Señor tu Dios con todo tu corazón, con toda tu alma y con todas tus fuerzas",
-			},
-			{
-				id: 3,
-				usuario: "Andrea Clemente",
-				rema:
-					"Ama al Señor tu Dios con todo tu corazón, con toda tu alma y con todas tus fuerzas",
-			},
-		],
-	});
-
 	return (
 		<div className='Muro'>
 			{/* <h2>Bienvenido {props.usuario}</h2> */}
 			<PublicarBox />
-			<RemaList remas={state.remas} />
+			<RemaList remas={props.remas} />
 		</div>
 	);
 };
--- src/containers/Inicio/Inicio.js
+++ src/containers/Inicio/Inicio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Inicio.css";
 
 // Components
@@ -7,11 +7,39 @@ import Muro from "../../components/Muro/Muro";
 import BarraAuxiliar from "../../components/UI/BarraAuxiliar/BarraAuxiliar";
 
 const Inicio = (props) => {
+	const [state, setState] = useState({
+		remas: [
+			{
+				id: 0,
+				usuario: "Josué Rauda",
+				rema: "Todo lo que respire alabe al Señor",
+			},
+			{
+				id: 1,
+				usuario: "Katya Carrillo",
+				rema:
+					"Anhelo el día que venga Jesús porque ese día llegaré a ver su rostro",
+			},
+			{
+				id: 2,
+				usuario: "Andrea Clemente",
+				rema:
+					"Ama al Señor tu Dios con todo tu corazón, con toda tu alma y con todas tus fuerzas",
+			},
+			{
+				id: 3,
+				usuario: "Andrea Clemente",
+				rema:
+					"Ama al Señor tu Dios con todo tu corazón, con toda tu alma y con todas tus fuerzas",
+			},
+		],
+	});
+
 	return (
 		<div className='Inicio'>
 			<BarraLateral />
 			<BarraAuxiliar />
-			<Muro usuario='Josué Rauda' />
+			<Muro remas={state.remas} usuario='Josué Rauda' />
 		</div>
 	);
 };
